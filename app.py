02e295921ea874a9799526ca14f61dcaab415766
Kenyi Hancco <kenyihq@gmail.com>
2022-05-31 12:21:55 -0500

    Added and run database connection

--- app.py
+++ app.py
@@ -1,9 +1,19 @@
 from flask import Flask
+from psycopg2 import connect
+
+# Import from file database.py
+from database import get_connection
 
 app = Flask(__name__)
 
 @app.route('/', )
-def hello_world():
+
+def home():
+    conn = get_connection()
+    cur = conn.cursor()
+    
+    result = cur.execute("SELECT 1 + 1")
+    print(result)
     return 'Hello World!'
 
 if __name__ == '__main__':
---
+++ database.py
@@ -0,0 +1,11 @@
+from psycopg2 import connect
+
+host = 'localhost'
+port = 5432
+dbname = 'userdb'
+user = 'postgres'
+password = 'tutankam0nK'
+
+def get_connection():
+    conn = connect(host=host, port=port, dbname=dbname, user=user, password=password)
+    return conn
