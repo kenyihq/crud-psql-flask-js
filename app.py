12b9940b0af0a3cec8d4b0431b5e1ac552d73c56
Kenyi Hancco <kenyihq@gmail.com>
2022-05-31 12:35:50 -0500

    Added method get, post, put and delete

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from flask import Flask
-from psycopg2 import connect
 
 # Import from file database.py
 from database import get_connection
@@ -15,6 +14,26 @@ def home():
     result = cur.execute("SELECT 1 + 1")
     print(result)
     return 'Hello World!'
+
+@app.get('/api/users')
+def get_users():    
+    return 'getting users'
+
+@app.post('/api/users')
+def create_user():
+    return 'creating users'
+
+@app.put('/api/users/1')
+def update_user():
+    return 'updating users'
+
+@app.delete('/api/users/1')
+def delete_user():
+    return 'deleting users'
+
+@app.get('/api/users/1')
+def get_user():
+    return 'getting users'
 
 if __name__ == '__main__':
     app.run(debug=True)
