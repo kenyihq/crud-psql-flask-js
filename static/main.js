cbc3000451084820772cdc9020c02e3d9a880e7c
Kenyi Hancco <kenyihq@gmail.com>
2022-06-01 18:52:50 -0500

    Added creating user frot and back

--- static/main.js
+++ static/main.js
@@ -1,1 +1,23 @@
-console.log('Hello World!');
+const userForm = document.querySelector("#userForm");
+
+userForm.addEventListener("submit", async (e) => {
+  e.preventDefault();
+
+  const username = userForm["username"].value;
+  const password = userForm["password"].value;
+  const email = userForm["email"].value;
+
+  const response = await fetch("/api/users", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      username,
+      password,
+      email,
+    }),
+  });
+  const data = await response.json();
+  console.log(data);
+});
